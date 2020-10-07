22f2a8c31dcfa9e0aa1c985747345688df69e90b
Victor Kavsjo <kavsjo88@hotmail.se>
2020-10-07 16:23:17 +0200

    first commit

---
+++ code-coverage-jacoco/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class FizzBuzzTest {
+
+
+
+}
